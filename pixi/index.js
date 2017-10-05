f6a8da1ad517f8c5b6b8eb184f7895ec274c63d5
Yi-Wen Lin <bongiovi015@gmail.com>
2017-10-05 18:07:41 +0100

    Pixi example

--- pixi/index.js
+++ pixi/index.js
@@ -1,12 +1,52 @@
 // index.js
 
 import * as PIXI from 'pixi.js';
-import Test from './Test';
 
-let a = 5;
-const b = 10;
+const renderer = PIXI.autoDetectRenderer(500, 500);
+document.body.appendChild(renderer.view);
+
+const stage = new PIXI.Stage();
+
+const d = .5;
+const positions = [
+
+	-d, 0, 0,
+	 d, 0, 0,
+	 d, d, 0
+];
+
+const indices = [0, 1, 2];
+const geometry = new PIXI.mesh.Geometry()
+					.addAttribute('aVertexPosition', positions)
+					.addIndex(indices);
+const vs = `
+	precision highp float;
+	attribute vec3 aVertexPosition;
+
+	void main() {
+		gl_Position = vec4(aVertexPosition, 1.0);
+	}
+`;		
+
+const fs = `
+	void main() {
+		gl_FragColor = vec4(1.0);
+	}
+`;
+
+const shader = PIXI.Shader.from(vs, fs);
+const mesh = new PIXI.mesh.RawMesh(geometry, shader);
+stage.addChild(mesh);
 
 
-console.log(a + b);
 
-console.log('PIXI :', PIXI.Container);
+
+loop();
+
+function loop() {
+	
+	renderer.render(stage);
+
+	requestAnimationFrame(loop);
+
+}
